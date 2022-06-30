64a83c60e357d337a269e819f831ffb3f387c6c7
Ken Fuchiwaki <kenfcw@outlook.jp>
2022-06-30 18:04:18 +0900

    test2

---
+++ Source/Tag_Rogue/Level/GameMap_Alpha.cpp
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "GameMap_Alpha.h"
+
+
+AGameMap_Alpha::AGameMap_Alpha()
+{
+	BP_Reference = TMap<FString, TSubclassOf<AActor>>();
+}
+
+
+void AGameMap_Alpha::BeginPlay()
+{
+	CellSize = 100.0;
+	Generator = NewObject<URogueAlpha_MapGenerator>(GetWorld());
+	Generator->Construct(50,50);
+	Generator->SetStructureParam(UMapGeneratorBase::EType::Plaza, 9, 9, 4);
+    Generator->SetStructureParam(UMapGeneratorBase::EType::Room, 5, 5, 9);
+	Generator->BuildMap();
+	AddClass(TEXT("MapUnitBase"), TEXT("/Game/MapObject/MapUnit/MapBasicWall.MapBasicWall_C"));
+	PlaceCubes();
+	Super::BeginPlay();
+}
+
+void AGameMap_Alpha::PlaceCubes()
+{
+	for(int32 y=0;y<Generator->MapHeight;y++)
+	{
+		for (int32 x=0;x<Generator->MapWidth;x++)
+		{
+			if (Generator->GetCell(y, x)->Attribution==UMapGeneratorBase::EType::Wall)
+			{
+				GetWorld()->SpawnActor<AActor>(GetClass(TEXT("MapUnitBase")), Cie_Convert(y,x,50), FRotator(0,0,0));
+			}
+			
+		}
+	}
+}
+
+
+void AGameMap_Alpha::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+}
+
+FVector AGameMap_Alpha::Cie_Convert(const int32 PosY, const int32 PosX, const float Height) const
+{
+	return FVector((PosX+0.5)*CellSize,(PosY+0.5)*CellSize,Height);
+}
+
+TArray<int32> AGameMap_Alpha::Cie_Invert(const FVector PosVector)
+{
+	return {FMath::RoundToInt32(PosVector.Y),FMath::RoundToInt32(PosVector.X)};
+}
+
+void AGameMap_Alpha::AddClass(const FString Name, const FString ReferencePath)
+{
+	BP_Reference.Add(FString(Name), TSoftClassPtr<AActor>(FSoftObjectPath(ReferencePath)).LoadSynchronous());
+}
+
+TSubclassOf<AActor> AGameMap_Alpha::GetClass(const FString Name)
+{
+	return BP_Reference[Name];
+}
--- Source/Tag_Rogue/Level/GameMap_Alpha.h
+++ Source/Tag_Rogue/Level/GameMap_Alpha.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Engine/LevelScriptActor.h"
+#include "Tag_Rogue/MapGenerator/RogueAlpha_MapGenerator.h"
 #include "GameMap_Alpha.generated.h"
 
 /**
@@ -15,12 +16,16 @@ class TAG_ROGUE_API AGameMap_Alpha : public ALevelScriptActor
 	GENERATED_BODY()
 protected:
 	float CellSize;
-	
+	UPROPERTY()
+	URogueAlpha_MapGenerator* Generator;
+	TMap<FString, TSubclassOf<AActor>> BP_Reference;
 	virtual void BeginPlay() override;
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 	FVector Cie_Convert(int32,int32, float) const;
 	static TArray<int32> Cie_Invert(FVector);
-	
+	TSubclassOf<AActor> GetClass(FString);
+	void AddClass(FString,FString);
+	void PlaceCubes();
 public:
 	AGameMap_Alpha();
 };
